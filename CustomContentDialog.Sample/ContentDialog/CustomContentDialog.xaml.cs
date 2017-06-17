978baa42832ea07763d2d91700c52d957cc488a3
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2017-06-17 16:43:36 +0430

    Add project files.

---
+++ CustomContentDialog.Sample/ContentDialog/CustomContentDialog.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CustomContentDialog.Sample.ContentDialog
+{
+    public sealed partial class CustomContentDialog : UserControl
+    {
+
+        #region Events
+
+        public event EventHandler Closed;
+
+        public event EventHandler Opened;
+
+        #endregion
+
+        #region Properties
+
+        public new static readonly DependencyProperty MarginProperty = DependencyProperty.Register(
+            "Margin", typeof(Thickness), typeof(CustomContentDialog), new PropertyMetadata(default(Thickness)));
+
+        public new Thickness Margin
+        {
+            get { return (Thickness)GetValue(MarginProperty); }
+            set
+            {
+                SetValue(MarginProperty, value);
+                Popup.VerticalOffset = Margin.Top;
+                Popup.HorizontalOffset = Margin.Right;
+            }
+        }
+
+
+        public static readonly DependencyProperty DialogBackgroundProperty = DependencyProperty.Register(
+            "DialogBackground", typeof(Brush), typeof(CustomContentDialog), new PropertyMetadata(default(Brush)));
+
+        public Brush DialogBackground
+        {
+            get { return (Brush)GetValue(DialogBackgroundProperty); }
+            set { SetValue(DialogBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty BlurBackgroundActiveProperty = DependencyProperty.Register(
+            "BlurBackgroundActive", typeof(bool), typeof(CustomContentDialog), new PropertyMetadata(default(bool)));
+
+        public bool BlurBackgroundActive
+        {
+            get { return (bool)GetValue(BlurBackgroundActiveProperty); }
+            set
+            {
+                SetValue(BlurBackgroundActiveProperty, value);
+                if (value)
+                {
+                    LayoutRoot.Background = new SolidColorBrush(Colors.Transparent);
+                    BackDrop.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    LayoutRoot.Background = DialogBackground;
+                    BackDrop.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        public new static readonly DependencyProperty ContentProperty = DependencyProperty.Register(
+            "Content", typeof(UIElement), typeof(CustomContentDialog), new PropertyMetadata(default(UIElement)));
+
+        public new UIElement Content
+        {
+            get { return (UIElement)GetValue(ContentProperty); }
+            set
+            {
+                SetValue(ContentProperty, value);
+                GridContent.Children.Add(value);
+            }
+        }
+
+        public static readonly DependencyProperty IsCanDismissWithClickOutProperty = DependencyProperty.Register(
+            "IsCanDismissWithClickOut", typeof(bool), typeof(CustomContentDialog), new PropertyMetadata(true));
+
+        public bool IsCanDismissWithClickOut
+        {
+            get { return (bool)GetValue(IsCanDismissWithClickOutProperty); }
+            set { SetValue(IsCanDismissWithClickOutProperty, value); }
+        }
+
+        public static readonly DependencyProperty HorizontalDialogContentAlignmentProperty = DependencyProperty.Register(
+            "HorizontalDialogContentAlignment", typeof(HorizontalAlignment), typeof(CustomContentDialog), new PropertyMetadata(Windows.UI.Xaml.HorizontalAlignment.Center));
+
+        public HorizontalAlignment HorizontalDialogContentAlignment
+        {
+            get { return (HorizontalAlignment)GetValue(HorizontalDialogContentAlignmentProperty); }
+            set
+            {
+                SetValue(HorizontalDialogContentAlignmentProperty, value);
+                GridContent.HorizontalAlignment = value;
+            }
+        }
+
+        public static readonly DependencyProperty VerticalDialogContentAlignmentProperty = DependencyProperty.Register(
+            "VerticalDialogContentAlignment", typeof(VerticalAlignment), typeof(CustomContentDialog), new PropertyMetadata(Windows.UI.Xaml.VerticalAlignment.Center));
+
+        public VerticalAlignment VerticalDialogContentAlignment
+        {
+            get { return (VerticalAlignment)GetValue(VerticalDialogContentAlignmentProperty); }
+            set
+            {
+                SetValue(VerticalDialogContentAlignmentProperty, value);
+                GridContent.VerticalAlignment = value;
+            }
+        }
+
+        public static readonly DependencyProperty BlurAmountProperty = DependencyProperty.Register(
+            "BlurAmount", typeof(double), typeof(CustomContentDialog), new PropertyMetadata(15));
+
+        public double BlurAmount
+        {
+            get { return (double)GetValue(BlurAmountProperty); }
+            set
+            {
+                SetValue(BlurAmountProperty, value);
+                BackDrop.BlurAmount = value;
+            }
+        }
+
+        #endregion
+
+        public CustomContentDialog()
+        {
+            this.InitializeComponent();
+
+            LayoutUpdated += OnLayoutUpdated;
+        }
+
+        private void OnLayoutUpdated(object sender, object o)
+        {
+            LayoutRoot.Width = GetScreenResolutionInfo().Width - (Margin.Right + Margin.Left);
+            LayoutRoot.Height = GetScreenResolutionInfo().Height - (Margin.Top + Margin.Bottom);
+
+            BackgroundGrid.Width = LayoutRoot.Width;
+            BackgroundGrid.Height = LayoutRoot.Height;
+        }
+
+        private void PopupOnClosed(object sender, object o)
+        {
+            Closed?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void PopupOnOpened(object sender, object o)
+        {
+            Opened?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Show()
+        {
+            if (Popup != null)
+            {
+                Popup.IsOpen = true;
+                FocusOnFirstTabStop();
+            }
+        }
+
+        public void Hide()
+        {
+            if (Popup != null)
+            {
+                Popup.IsOpen = false;
+            }
+        }
+
+        private void FocusOnFirstTabStop()
+        {
+            var control = FocusManager.FindFirstFocusableElement(Content);
+            if (control is Control)
+            {
+                var tabControl = control as Control;
+                tabControl.Focus(FocusState.Programmatic);
+            }
+        }
+
+        public static Size GetScreenResolutionInfo()
+        {
+            var size = new Size(((Frame)Window.Current.Content).ActualWidth, ((Frame)Window.Current.Content).ActualHeight);
+            return size;
+        }
+
+
+        private void LayoutRoot_OnPointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (IsCanDismissWithClickOut)
+            {
+                Hide();
+            }
+        }
+
+        protected override void OnPointerReleased(PointerRoutedEventArgs e)
+        {
+            base.OnPointerReleased(e);
+            if (IsCanDismissWithClickOut)
+            {
+                Hide();
+            }
+        }
+    }
+}
---
+++ CustomContentDialog.Sample/MainPage.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel.Channels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CustomContentDialog.Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private ContentDialog.CustomContentDialog _dialog;
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _dialog = new ContentDialog.CustomContentDialog
+            {
+                HorizontalDialogContentAlignment = HorizontalAlignment.Center,
+                VerticalDialogContentAlignment = VerticalAlignment.Center,
+                Content = GenerateContent(),
+                BlurBackgroundActive = false,
+                DialogBackground = new SolidColorBrush(Colors.AntiqueWhite),
+                BlurAmount = 13
+            };
+
+            _dialog.Show();
+        }
+
+        private UIElement GenerateContent()
+        {
+            var grid = new Grid()
+            {
+                FlowDirection = FlowDirection.RightToLeft,
+                Background = new SolidColorBrush(Colors.LightGreen),
+                Height = 224,
+                BorderThickness = new Thickness(1),
+                BorderBrush = new SolidColorBrush(Colors.LightGray)
+            };
+
+            grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
+
+                var title = new TextBlock()
+                {
+                    Text = "Welcome",
+                    Margin = new Thickness(5, 36, 5, 5),
+                    TextWrapping = TextWrapping.Wrap,
+                    FontSize = 20,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Foreground = new SolidColorBrush(Colors.White)
+                };
+
+                grid.Children.Add(title);
+                Grid.SetRow(title, 0);
+            
+
+            var message = new TextBlock()
+            {
+                Text = "Hi guys, This is Custom content dialog control.",
+                Margin = new Thickness(15, 35, 15, 5),
+                FontSize = 18,
+                TextWrapping = TextWrapping.Wrap,
+                TextAlignment = TextAlignment.Right,
+                FlowDirection = FlowDirection.LeftToRight,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Top,
+                Foreground = new SolidColorBrush(Colors.White)
+            };
+
+            grid.Children.Add(message);
+            Grid.SetRow(message, 1);
+
+            var girBotton = new Grid()
+            {
+                Background = new SolidColorBrush(Colors.White)
+            };
+
+            girBotton.ColumnDefinitions.Add(new ColumnDefinition());
+            girBotton.ColumnDefinitions.Add(new ColumnDefinition());
+
+            var btnYes = new Button()
+            {
+                Content ="Yes",
+                Background = new SolidColorBrush(Colors.White),
+                Foreground = new SolidColorBrush(Colors.DodgerBlue),
+                Padding = new Thickness(4),
+                BorderThickness = new Thickness(1, 0, 0, 0),
+                BorderBrush = new SolidColorBrush(Color.FromArgb(27, 192, 192, 192)),
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Margin = new Thickness(0),
+                FontSize = 14,
+                Height = 54,
+                Width = 160
+            };
+
+            btnYes.Click += delegate (object sender, RoutedEventArgs args)
+            {
+                _dialog.Hide();
+            };
+
+            girBotton.Children.Add(btnYes);
+            Grid.SetColumn(btnYes, 1);
+
+            var btnNo = new Button()
+            {
+                Content = "No",
+                BorderThickness = new Thickness(0),
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Padding = new Thickness(4),
+                Margin = new Thickness(0),
+                FontSize = 14,
+                Background = new SolidColorBrush(Colors.White),
+                Foreground = new SolidColorBrush(Colors.DarkRed),
+                Height = 54,
+                Width = 160
+            };
+
+            btnNo.Click += delegate (object sender, RoutedEventArgs args)
+            {
+                _dialog.Hide();
+            };
+
+            girBotton.Children.Add(btnNo);
+            Grid.SetColumn(btnNo, 0);
+
+            grid.Children.Add(girBotton);
+            Grid.SetRow(girBotton, 2);
+
+            return grid;
+        }
+    }
+}
